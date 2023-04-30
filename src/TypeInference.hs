bb7c43e09542c8fde3bd7c831b449a8917b0b15d
Faiz Ilham <faizilham.faizz@gmail.com>
2023-04-30 17:19:38 +0200

    update to LabelledType

--- src/TypeInference.hs
+++ src/TypeInference.hs
@@ -5,51 +5,44 @@ module TypeInference (
 import AST
 import Type
 
-import Data.Map (Map, empty, lookup, fromList, toList, union)
+import Data.Map as Map (Map, empty, lookup, fromList, toList, union)
 import Control.Monad.State
 import Control.Monad.Except
 
 type TypeEnvironment = Map Id TypeScheme
 
-type Substitution = Map TypeVar Type
-
--- TODO: fill. implement for Nat, Bool, Fun and Var for now
-substitute :: Substitution -> Type -> Type
-substitute substMap (TVar v) = case Data.Map.lookup v substMap of
-                                 Just t -> t
-                                 Nothing -> TVar v
-substitute substMap (TFun lt1 lt2) = TFun (substituteLT substMap lt1) (substituteLT substMap lt2)
-substitute substMap t = t
+type Substitution = Map TypeVar LabelledType
 
 -- substitute labelled type
-substituteLT :: Substitution -> LabelledType -> LabelledType
-substituteLT substMap (LabelledType t l) = LabelledType (substitute substMap t) l
+substitute :: Substitution -> LabelledType -> LabelledType
+substitute substMap (LabelledType t l) = substituteType substMap t l -- TODO: handle label?
+
+substituteType :: Substitution -> Type -> Label -> LabelledType
+substituteType substMap (TVar v)       label = case Map.lookup v substMap of
+                                                  Just t -> t
+                                                  Nothing -> LabelledType (TVar v) label -- TODO: should this be error?
+substituteType substMap (TFun lt1 lt2) label = LabelledType (TFun (substitute substMap lt1) (substitute substMap lt2)) label
+substituteType substMap t              label = LabelledType t label -- TODO: TArray, TPair, TList
 
 -- substitute type stored in the env
 substituteEnv :: Substitution -> TypeEnvironment -> TypeEnvironment
-substituteEnv substMap typeEnv = fromList (substitueTypeList substMap (toList typeEnv))
+substituteEnv substMap typeEnv = fromList (substituteTypeList substMap (toList typeEnv))
 
 -- substitute type stored in a list
-substitueTypeList :: Substitution -> [(Id, TypeScheme)] -> [(Id, TypeScheme)]
-substitueTypeList substMap (t:ts) = case t of
-  (id, ts') -> [(id, substitueTS substMap ts')] ++ (substitueTypeList substMap ts)
+substituteTypeList :: Substitution -> [(Id, TypeScheme)] -> [(Id, TypeScheme)]
+substituteTypeList substMap = map update
+                              where update (id, ts) = (id, substituteTS substMap ts)
 
 -- substitute type in TypeScheme
-substitueTS :: Substitution -> TypeScheme -> TypeScheme
-substitueTS substMap (Forall v ts) = Forall v (substitueTS substMap ts)
-substitueTS substMap (Type lt) = Type (substituteLT substMap lt)
+substituteTS :: Substitution -> TypeScheme -> TypeScheme
+substituteTS substMap (Forall v ts) = Forall v (substituteTS substMap ts)
+substituteTS substMap (Type lt) = Type (substitute substMap lt)
 
 infixr 9 .+
--- TODO: fill, substitution composition
 -- substMapNew should be obtained after substMapOld
 (.+) :: Substitution -> Substitution -> Substitution
-(.+) substMapNew substMapOld = union substMapNew (fromList (updateOldSubstList (toList substMapOld) substMapNew))
-
--- update the old Substitution in form of list with new Substitution
-updateOldSubstList :: [(TypeVar, Type)] -> Substitution -> [(TypeVar, Type)]
-updateOldSubstList (s:ss) substMapNew = case s of
-  (v, TVar v') -> [(v, substitute substMapNew (TVar v'))] ++ updateOldSubstList ss substMapNew
-  (v, TFun lt1 lt2) -> [(v, substitute substMapNew (TFun lt1 lt2))] ++ updateOldSubstList ss substMapNew
+(.+) substMapNew substMapOld = substMapNew `union` fmap update substMapOld
+                               where update = substitute substMapNew
 
 newtype InferenceContext = InferenceContext { currentTypeVar :: Int }
 
@@ -72,24 +65,21 @@ fresh = do
           return (TypeVar current)
 
 -- generalize function of W Algorithm
-generalize :: TypeEnvironment -> Type -> InferenceState TypeScheme
+generalize :: TypeEnvironment -> LabelledType -> InferenceState TypeScheme
 generalize = undefined -- TODO: fill. implement for Nat, Bool, Fun and Var for now
 
 -- instantiate function of W Algorithm
-instantiate :: TypeScheme -> InferenceState Type
+instantiate :: TypeScheme -> InferenceState LabelledType
 instantiate = undefined -- TODO: fill
 
 -- unify (U) function of W Algorithm
-unify :: Type -> Type -> InferenceState Substitution
+unify :: LabelledType -> LabelledType -> InferenceState Substitution
 unify = undefined -- TODO: fill. implement for Nat, Bool, Fun and Var for now
 
 -- W function of W Algorithm
-wAlg :: TypeEnvironment -> Expr -> InferenceState (Type, Substitution)
+wAlg :: TypeEnvironment -> Expr -> InferenceState (LabelledType, Substitution)
 wAlg = undefined -- TODO: fill
 
-toTypeScheme :: Type -> TypeScheme
-toTypeScheme t = Type $ LabelledType t L -- TODO: use proper label analysis instead of this?
-
 infer :: Expr -> Either String TypeScheme
-infer e = fmap (toTypeScheme . fst) result
+infer e = fmap (Type . fst) result
           where result = evalInference (wAlg empty e) newContext
